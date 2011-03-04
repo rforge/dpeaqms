dd2dbfa69dd76c31e089d0cf71865db419379fc8
hjow <hjow@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-04 17:15:44 +0000

    Modified dpeaqms.csv2msms to allow multiple csvfiles
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dpeaqms@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/dpeaqms.csv2msms.r
+++ pkg/R/dpeaqms.csv2msms.r
@@ -1,67 +1,75 @@
 # Converts a CSV file into an msms data frame usable by other functions that require 
 # msms data
-dpeaqms.csv2msms<-function(csvfile=NULL, proteinColumn=1 , msmsidColumn=NULL,
+dpeaqms.csv2msms<-function(csvfiles=NULL, proteinColumn=1 , msmsidColumn=NULL,
                            reporterIonNames=c(126,127,128,129,130,131) ,                           
                            sampleGroupAssignment=c("A","A","A","B","B","B"),
                            includeUnknownProteins=T) {
  
-  if (!is.null(csvfile)) {
-       
-    csvdata <- read.table(csvfile, header=T, sep=",", as.is=T)
-    expsize = dim(csvdata)
-    numObservations = expsize[1]*length(reporterIonNames)
-    experimentID = rep(1, numObservations)
-    proteinID    = rep("", numObservations)
-    intensity    = rep(0.0,  numObservations)
-    sampleID     = rep("", numObservations)
-    groupID      = rep("", numObservations)
-    msmsID       = rep("", numObservations)
-    
-    sampleNamesR = paste("X", reporterIonNames, sep="") ;
-    ionIntensityColumns = match(sampleNamesR, colnames(csvdata))
+  if (!is.null(csvfiles)) {
+    proteinID    = c()
+    intensity    = c()
+    sampleID     = c()
+    groupID      = c()
+    msmsID       = c()
+    experimentID = c()
+    for (d in seq(1,length(csvfiles))) {
+      csvdata <- read.table(csvfiles[d], header=T, sep=",", as.is=T)
+      offset = length(experimentID)
+      expsize = dim(csvdata)
+      numObservations = expsize[1]*length(reporterIonNames)
+      experimentID = c(experimentID,rep(d, numObservations))
+      proteinID    = c(proteinID, rep("", numObservations))
+      intensity    = c(intensity, rep(0.0,  numObservations))
+      sampleID     = c(sampleID, rep("", numObservations))
+      groupID      = c(groupID,rep("", numObservations))
+      msmsID       = c(msmsID,rep("", numObservations))
+      
+      sampleNamesR = paste("X", reporterIonNames, sep="") ;
+      ionIntensityColumns = match(sampleNamesR, colnames(csvdata))
    
-    for (i in seq(1,length(ionIntensityColumns))) {
-      if (is.nan(ionIntensityColumns[i])) {
-        print(paste("Error: No column \"" , reporterIonNames[i] , "\" in " , csvfile))
-        return(NULL)
+      for (i in seq(1,length(ionIntensityColumns))) {
+        if (is.nan(ionIntensityColumns[i])) {
+          print(paste("Error: No column \"" , reporterIonNames[i] , "\" in " , csvfiles[d]))
+          return(NULL)
+        }
       }
-    }
    
-    if (is.null(msmsidColumn)) {
-      msmsName = seq(1,expsize[1])
-    }
-    else {
-      msmsName = csvdata[,msmsidColumn[1]]
-      for (z in seq(2,length(msmsidColumn))) {
-        temp = csvdata[,msmsidColumn[z]]
-        temp = gsub('[)$]', '' , temp)
-        temp = gsub('[(|)|\"]', '.' , temp)
-        temp = gsub('[ ]+','',temp)          
-        msmsName = paste(msmsName, temp, sep=".")
+      if (is.null(msmsidColumn)) {
+        msmsName = seq(1,expsize[1])
       }
-      msmsName = paste(msmsName, seq(1,expsize[1]), sep = ".")
-    }
-    proteinAccession = csvdata[,proteinColumn]
-    proteinAccession = gsub('[-*+/]+', '.', proteinAccession)
+      else {
+        msmsName = csvdata[,msmsidColumn[1]]
+        for (z in seq(2,length(msmsidColumn))) {
+          temp = csvdata[,msmsidColumn[z]]
+          temp = gsub('[)$]', '' , temp)
+          temp = gsub('[(|)|\"]', '.' , temp)
+          temp = gsub('[ ]+','',temp)          
+          msmsName = paste(msmsName, temp, sep=".")
+        }
+        msmsName = paste(msmsName, seq(1,expsize[1]), sep = ".")
+      }
+      proteinAccession = csvdata[,proteinColumn]
+      proteinAccession = gsub('[-*+/]+', '.', proteinAccession)
     
-    proteinAccession[proteinAccession==""] = "Unknown"
-    experientID = rep(1,expsize[1])
-    numSamples = length(reporterIonNames)
-    for (i in seq(1,expsize[1])) {
-      L = (i-1)*numSamples+1
-      U = i*numSamples
-      #print(proteinAccession[i])
-      proteinID[L:U] = proteinAccession[i]
-      for (j in seq(0,numSamples-1)) {
-        intensity[L+j] = csvdata[i,ionIntensityColumns[j+1]]
+      proteinAccession[proteinAccession==""] = "Unknown"
+      experientID = rep(1,expsize[1])
+      numSamples = length(reporterIonNames)
+      for (i in seq(1,expsize[1])) {
+        L = offset+(i-1)*numSamples+1
+        U = offset+i*numSamples
+      
+        proteinID[L:U] = proteinAccession[i]
+        for (j in seq(0,numSamples-1)) {
+          intensity[L+j] = csvdata[i,ionIntensityColumns[j+1]]
+        }
+        sampleID[L:U]  = reporterIonNames
+        groupID[L:U]   = sampleGroupAssignment
+        msmsID[L:U]    = msmsName[i]
       }
-      sampleID[L:U]  = reporterIonNames
-      groupID[L:U]   = sampleGroupAssignment
-      msmsID[L:U]    = msmsName[i]
     }
   }
   else {
-    print("Error: No csvfile specified")
+    print("Error: No csvfiles specified")
     return (NULL) 
   }
 
